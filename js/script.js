7127fdfeb1f80655a43045de9b0650c9e61f2ed5
Lera Merzlyakova <sniff451@gmail.com>
2018-10-22 19:12:18 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".menu__item").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
